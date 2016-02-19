c2be07afa18ce8891c3f08ff39db25855fb9adf1
Eric Conde <econde@stratpoint.com>
2016-02-19 09:27:45 +0800

    add example

--- 27_monkey_patching.rb
+++ 27_monkey_patching.rb
@@ -7,6 +7,12 @@ class Object
     puts to_s
     puts "#"*20
   end
+
+  def frozen?
+    p "Elsa: Let it go... Let it go!"
+    super
+
+  end
 end
 
 puts "called on a String object"
@@ -14,3 +20,8 @@ puts "called on a String object"
 puts
 puts "called on a Time object"
 Time.now.pretty_to_s
+puts
+# show-method
+puts "Override Object#frozen? method"
+result = "Elsa".frozen?
+puts "Frozen? #{result}"
